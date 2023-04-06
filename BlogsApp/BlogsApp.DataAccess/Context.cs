aab24d29be8ce12e457b9a8a8fa22eb35253d805
carmesotuyo <carmela.sotuyo@gmail.com>
2023-04-06 12:59:21 -0300

    fresh

---
+++ BlogsApp/BlogsApp.DataAccess/Context.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogsApp.DataAccess
+{
+	public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options) { }
+    }
+}
+
---
+++ BlogsApp/BlogsApp.WebAPI/Program.cs
@@ -0,0 +1,26 @@
+﻿using BlogsApp.DataAccess; // creo que la WebAPI no deberia depender del DataAccess, pero fue lo que hicimos en clase
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<Context>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
