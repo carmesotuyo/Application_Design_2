c29aa1fbd042176b3dcfcda22deb21169287a3f3
carmesotuyo <carmela.sotuyo@gmail.com>
2023-04-21 18:31:26 -0300

    added Factory for services

---
+++ Codigo/Backend/BlogsApp.Factory/ServiceFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BlogsApp.BusinessLogic;
+using BlogsApp.BusinessLogic.Logics;
+using BlogsApp.IBusinessLogic;
+using BlogsApp.IBusinessLogic.Interfaces;
+
+namespace BlogsApp.Factory
+{
+    public class ServiceFactory
+    {
+        private readonly IServiceCollection services;
+
+        public ServiceFactory(IServiceCollection services)
+        {
+            this.services = services;
+        }
+
+        public void AddCustomServices()
+        {
+            services.AddScoped<IArticleLogic, ArticleLogic>();
+        }
+    }
+}
+
---
+++ Codigo/Backend/BlogsApp.WebAPI/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BlogsApp.Factory;
+
+namespace BlogsApp.WebAPI
+{
+	public class Startup
+	{
+		public Startup()
+		{
+		}
+
+        //...STARTUP CODE
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //...CONFIGURE_SERVICES CODE
+
+            ServiceFactory factory = new ServiceFactory(services);
+        }
+    }
+}
+}
+
