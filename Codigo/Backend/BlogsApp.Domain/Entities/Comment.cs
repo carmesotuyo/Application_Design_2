93c3d32548cf27bfb273dd930d607a5e6110bb7f
carmesotuyo <carmela.sotuyo@gmail.com>
2023-04-22 19:53:57 -0300

    edited Comment properties

--- Codigo/Backend/BlogsApp.Domain/Entities/Comment.cs
+++ Codigo/Backend/BlogsApp.Domain/Entities/Comment.cs
@@ -4,13 +4,17 @@
     {
         public User User { get; set; }
         public string Body { get; set; }
-        public Reply reply { get; set; }
+        public Reply? Reply { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
-        public Boolean delete { get; set; }
+        public DateTime? DateDeleted { get; set; }
 
-        public Comment()
+        public Comment(User user, string body)
         {
+            User = user;
+            Body = body;
+            DateCreated = DateTime.Now;
+            DateModified = DateTime.Now;
         }
     }
 }
