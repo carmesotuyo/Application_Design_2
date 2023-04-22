a4eb6c4e99ae67b609984c7d2538612152968e48
carmesotuyo <carmela.sotuyo@gmail.com>
2023-04-22 19:38:19 -0300

    edited Articles properties and constructor

--- Codigo/Backend/BlogsApp.Domain/Entities/Article.cs
+++ Codigo/Backend/BlogsApp.Domain/Entities/Article.cs
@@ -8,15 +8,22 @@
         public bool Private { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
-        public DateTime DateDeleted { get; set; }
+        public Nullable<DateTime> DateDeleted { get; set; }
         public User User { get; set; }
         public List<Comment> Comments;
-        public IEnumerable<int> Template { get; set; }
-        public string Image { get; set; }
+        public int Template { get; set; }
+        public Nullable<string> Image { get; set; }
 
 
-        public Article()
+        public Article(string name, string body, int template, User user)
         {
+            Name = name;
+            Body = body;
+            DateCreated = DateTime.Now;
+            DateModified = DateTime.Now;
+            User = user;
+            Comments = new List<Comment>();
+            Template = template;
         }
     }
 }
