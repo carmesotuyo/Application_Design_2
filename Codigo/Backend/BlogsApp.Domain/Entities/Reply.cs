efd0816ab78bd7b0a3473090021a3b8190138a8d
carmesotuyo <carmela.sotuyo@gmail.com>
2023-04-22 19:55:23 -0300

    edited Reply properties

--- Codigo/Backend/BlogsApp.Domain/Entities/Reply.cs
+++ Codigo/Backend/BlogsApp.Domain/Entities/Reply.cs
@@ -6,9 +6,14 @@
         public string Body { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
-        public Boolean delete { get; set; }
-        public Reply()
+        public DateTime? DateDeleted { get; set; }
+
+        public Reply(User user, string body)
         {
+            User = user;
+            Body = body;
+            DateCreated = DateTime.Now;
+            DateModified = DateTime.Now;
         }
     }
 }
