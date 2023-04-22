585874d3709e5eff5304eaaded2b807d0daf119d
carmesotuyo <carmela.sotuyo@gmail.com>
2023-04-22 11:41:10 -0300

    deleted startup file and fixed program

--- Codigo/Backend/BlogsApp.WebAPI/Program.cs
+++ Codigo/Backend/BlogsApp.WebAPI/Program.cs
@@ -1,33 +1,29 @@
 ﻿//using BlogsApp.DataAccess; // WEBAPI YA NO DEPENDE DE DATA ACCESS, DESACOPLAMOS DE LA IMPLEMENTACION
+using BlogsApp.Factory;
 using BlogsApp.WebAPI;
 
 var builder = WebApplication.CreateBuilder(args);
-var startup = new Startup(builder.Configuration);
-startup.ConfigureServices(builder.Services);
-
-// COSAS QUE SE PASARON A STARTUP:
 
 // Add services to the container.
-//builder.Services.AddControllers();
+builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 //builder.Services.AddDbContext<Context>();
+ServiceFactory factory = new ServiceFactory(builder.Services);
+factory.AddCustomServices();
+factory.AddDbContextService(builder.Configuration.GetConnectionString("BlogsAppDBCarme"));
 
 var app = builder.Build();
-startup.Configure(app);
 
-//CREO QUE DEBERIA IR EN STARTUP PERO NO ME ANDA ASI QUE LO DEJO ACA:
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-//ESTO ACA ESTA BIEN:
 
 app.UseAuthorization();
 
