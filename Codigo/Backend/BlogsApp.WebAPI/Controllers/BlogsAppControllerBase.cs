63979d2d011fd8e9d18b38d4438f0a68a3361265
carmesotuyo <carmela.sotuyo@gmail.com>
2023-04-30 01:37:08 -0300

    add exception filter

--- Codigo/Backend/BlogsApp.WebAPI/Controllers/BlogsAppControllerBase.cs
+++ Codigo/Backend/BlogsApp.WebAPI/Controllers/BlogsAppControllerBase.cs
@@ -1,8 +1,10 @@
+using BlogsApp.WebAPI.Filters;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogsApp.WebAPI.Controllers
 {
     [ApiController]
+    [ServiceFilter(typeof(ExceptionFilter))]
     public class BlogsAppControllerBase : ControllerBase
     {
     }
---
+++ Codigo/Backend/BlogsApp.WebAPI/Filters/ExceptionFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+namespace BlogsApp.WebAPI.Filters
+{
+	public class ExceptionFilter : Attribute, IExceptionFilter
+	{
+        public void OnException(ExceptionContext context)
+        {
+            try
+            {
+                throw context.Exception;
+            }
+            // completar con las exceptions que vayamos creando
+            catch (Exception)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 500,
+                    Content = "Unexpected error -- " + context.Exception.Message
+                };
+            }
+        }
+    }
+}
+
