0dacc548b1558ecd29566b558dc11af9695364ae
carmesotuyo <carmela.sotuyo@gmail.com>
2023-04-30 19:46:49 -0300

    red/create_comment

---
+++ Codigo/Backend/WebApi.Test/CommentControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using BlogsApp.IBusinessLogic.Interfaces;
+using BlogsApp.Domain.Entities;
+using BlogsApp.WebAPI.Controllers;
+
+namespace WebApi.Test
+{
+	[TestClass]
+	public class CommentControllerTest
+    {
+        Mock<ICommentLogic> commentLogicMock;
+        private CommmentController controller;
+
+        static readonly Comment comment = new Comment() { };
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            commentLogicMock = new Mock<ICommentLogic>(MockBehavior.Strict);
+            controller = new CommentController(commentLogicMock.Object);
+        }
+
+        [TestMethod]
+		public void CreateComment()
+		{
+            commentLogicMock.Setup(m => m.CreateComment().Returns(comment);
+
+            var result = controller.AddComment(comment);
+
+            commentLogicMock.VerifyAll();
+            OkObjectResult objectResult = result as OkObjectResult;
+
+            Assert.IsNotNull(objectResult);
+
+            Assert.IsTrue(objectResult.Equals(comment));
+        }
+	}
+}
+
