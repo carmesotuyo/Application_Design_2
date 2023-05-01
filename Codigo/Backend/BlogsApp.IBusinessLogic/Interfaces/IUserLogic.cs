e107976feb254dfc39b9231f1c512b06f9084e00
Gimena Alamón <gimenaalamon@gmail.com>
2023-05-01 01:41:44 -0300

    Tdd user data access

--- Codigo/Backend/BlogsApp.IBusinessLogic/Interfaces/IUserLogic.cs
+++ Codigo/Backend/BlogsApp.IBusinessLogic/Interfaces/IUserLogic.cs
@@ -5,7 +5,7 @@ namespace BlogsApp.IBusinessLogic.Interfaces
     public interface IUserLogic
     {
         User? InsertUser(User? user);
-        //public User UpdateUser(User user);
+        User? UpdateUser(User user);
         //public User DeleteUser(User user);
         //public User GetUserById(int id);
     }
