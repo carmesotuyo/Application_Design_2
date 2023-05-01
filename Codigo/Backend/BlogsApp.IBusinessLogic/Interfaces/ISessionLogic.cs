cb27125586581b578a8fd69b134b1fcfeb49c01f
carmesotuyo <carmela.sotuyo@gmail.com>
2023-05-01 19:44:30 -0300

    green/session_login_api

--- Codigo/Backend/BlogsApp.IBusinessLogic/Interfaces/ISessionLogic.cs
+++ Codigo/Backend/BlogsApp.IBusinessLogic/Interfaces/ISessionLogic.cs
@@ -1,7 +1,11 @@
-﻿namespace BlogsApp.IBusinessLogic.Interfaces
+﻿using BlogsApp.Domain.Entities;
+
+namespace BlogsApp.IBusinessLogic.Interfaces
 {
     public interface ISessionLogic
     {
         bool IsValidToken(string token);
+        Guid Login(string username, string password);
+        void Logout(User loggedUser);
     }
 }
