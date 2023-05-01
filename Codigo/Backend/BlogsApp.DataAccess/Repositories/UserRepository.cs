9c40a12113ee8a79cdc04fe0379f3f7917b0c5f3
Fernando Spillere <f.spillere269@gmail.com>
2023-05-01 06:27:04 -0300

    Fixes en userRepository

--- Codigo/Backend/BlogsApp.DataAccess/Repositories/UserRepository.cs
+++ Codigo/Backend/BlogsApp.DataAccess/Repositories/UserRepository.cs
@@ -3,6 +3,8 @@ using BlogsApp.IDataAccess;
 using BlogsApp.IDataAccess.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using BlogsApp.DataAccess;
+using BlogsApp.DataAccess.Interfaces.Exceptions;
+using System.Linq;
 
 namespace BlogsApp.DataAccess.Repositories
 {
@@ -10,38 +12,53 @@ namespace BlogsApp.DataAccess.Repositories
     {
         //private readonly DbSet<User> users;
 
-        private readonly Context context;
+        //private readonly Context context;
+        private DbContext Context { get; }
 
         public UserRepository(Context context)
         {
-            context = context;
+            Context = context;
         }
 
-        public User Add(User user)
+        public User Add(User value)
         {
-            context.Users?.Add(user);
-            context.SaveChanges();
-            return user;
+            bool exists = Context.Set<User>().Where(i => i.Id == value.Id).Any();
+            if (exists)
+                throw new AlreadyExistsDbException();
+            Context.Set<User>().Add(value);
+            Context.SaveChanges();
+            return value;
         }
 
         public void Update(User value)
         {
-            throw new NotImplementedException();
+            bool exists = Context.Set<User>().Where(i => i.Id == value.Id).Any();
+            if (!exists)
+                throw new NotFoundDbException();
+            User original = Context.Set<User>().Find(value.Id);
+            Context.Entry(original).CurrentValues.SetValues(value);
+            Context.SaveChanges();
         }
 
         public User Get(Func<User, bool> func)
         {
-            throw new NotImplementedException();
+            User user = Context.Set<User>().Where(a => a.DateDeleted == null).FirstOrDefault(func);
+            if (user == null)
+                throw new NotFoundDbException();
+            return user;
         }
 
         public ICollection<User> GetAll(Func<User, bool> func)
         {
-            throw new NotImplementedException();
+            ICollection<User> users = Context.Set<User>().Where(func).ToArray();
+            if (users.Count == 0)
+                throw new NotFoundDbException();
+            return users;
         }
 
         public bool Exists(Func<User, bool> func)
         {
-            throw new NotImplementedException();
+            return Context.Set<User>().Where(func).Any();
         }
     }
 }
