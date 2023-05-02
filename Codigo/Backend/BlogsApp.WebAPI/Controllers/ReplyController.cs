6f475746894c5ec357b5868e0dd2b268060b7860
carmesotuyo <carmela.sotuyo@gmail.com>
2023-05-02 02:01:34 -0300

    tdd/reply_controller

---
+++ Codigo/Backend/BlogsApp.WebAPI/Controllers/ReplyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BlogsApp.BusinessLogic.Logics;
+using BlogsApp.Domain.Entities;
+using BlogsApp.IBusinessLogic.Interfaces;
+using BlogsApp.WebAPI.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogsApp.WebAPI.Controllers
+{
+    [Route("api/replies")]
+    [ServiceFilter(typeof(AuthorizationFilter))]
+    public class ReplyController : BlogsAppControllerBase
+    {
+        private readonly IReplyLogic replyLogic;
+
+        public ReplyController(IReplyLogic replyLogic)
+        {
+            this.replyLogic = replyLogic;
+        }
+
+
+        [HttpPost]
+        public IActionResult PostReply([FromBody] Reply reply)
+        {
+            User loggedUser = (User)this.HttpContext.Items["user"];
+
+            return new OkObjectResult(replyLogic.CreateReply(reply, loggedUser));
+        }
+    }
+}
+
