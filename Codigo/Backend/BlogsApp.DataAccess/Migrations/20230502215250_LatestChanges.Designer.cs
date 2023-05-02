374b8ad4e052bee093afc26a06cf9be2da300807
carmesotuyo <carmela.sotuyo@gmail.com>
2023-05-02 20:02:54 -0300

    added migration

--- Codigo/Backend/BlogsApp.DataAccess/Migrations/20230502215250_LatestChanges.Designer.cs
+++ Codigo/Backend/BlogsApp.DataAccess/Migrations/20230502215250_LatestChanges.Designer.cs
@@ -4,6 +4,7 @@ using BlogsApp.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogsApp.DataAccess.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230502215250_LatestChanges")]
+    partial class LatestChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
