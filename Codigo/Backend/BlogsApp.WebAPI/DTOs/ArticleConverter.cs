fbd4227947c8be7fa0efc06c9dd723b743356b41
Fernando Spillere <f.spillere269@gmail.com>
2023-05-03 19:25:15 -0300

    Se agregan dtos

---
+++ Codigo/Backend/BlogsApp.WebAPI/DTOs/ArticleConverter.cs
@@ -0,0 +1,38 @@
+﻿using BlogsApp.Domain.Entities;
+
+namespace BlogsApp.WebAPI.DTOs
+{
+    public class ArticleConverter
+    {
+        public static Article FromDto(BasicArticleDto dto, User user)
+        {
+            return new Article(dto.Name, dto.Body, dto.Template, user)
+            {
+                Private = dto.Private,
+                Image = dto.Image
+            };
+        }
+        public static BasicArticleDto ToDto(Article article)
+        {
+            return new BasicArticleDto
+            {
+                Id = article.Id,
+                Name = article.Name,
+                Body = article.Body,
+                Private = article.Private,
+                Template = article.Template,
+                Image = article.Image
+            };
+        }
+
+        public static IEnumerable<BasicArticleDto> ToDtoList(IEnumerable<Article> articles)
+        {
+            List<BasicArticleDto> basicArticleDtos = new List<BasicArticleDto>();
+            foreach (Article article in articles)
+            {
+                basicArticleDtos.Add(ToDto(article));
+            }
+            return basicArticleDtos;
+        }
+    }
+}
--- Codigo/Backend/BlogsApp.WebAPI/DTOs/CreateArticleRequestDto.cs
+++ Codigo/Backend/BlogsApp.WebAPI/DTOs/CreateArticleRequestDto.cs
@@ -2,7 +2,7 @@
 
 namespace BlogsApp.WebAPI.DTOs
 {
-    public class CreateArticleRequestDto
+    public class BasicArticleDto
     {
         public int? Id { get; set; }
         public string Name { get; set; }
@@ -11,32 +11,4 @@ namespace BlogsApp.WebAPI.DTOs
         public int Template { get; set; }
         public string? Image { get; set; }
     }
-
-    public class ArticleConverter
-    {
-        public static Article FromDto(CreateArticleRequestDto dto, User user)
-        {
-            return new Article(dto.Name, dto.Body, dto.Template, user)
-            {
-                Private = dto.Private,
-                Image = dto.Image
-            };
-        }
-
-
-
-        public static CreateArticleRequestDto ToDto(Article article)
-        {
-            return new CreateArticleRequestDto
-            {
-                Id = article.Id,
-                Name = article.Name,
-                Body = article.Body,
-                Private = article.Private,
-                Template = article.Template,
-                Image = article.Image
-            };
-        }
-    }
-
 }
