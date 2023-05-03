94811c00ab1ea9476e5b2a74ba0bd68514169d95
Fernando Spillere <f.spillere269@gmail.com>
2023-05-02 22:17:52 -0300

    se crea entidad logEntry

---
+++ Codigo/Backend/BlogsApp.Logging/Entities/LogEntry.cs
@@ -0,0 +1,19 @@
+﻿using BlogsApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogsApp.Logging.Entities
+{
+    public class LogEntry
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public User User { get; set; }
+        public string ActionType { get; set; }
+        public string SearchQuery { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
