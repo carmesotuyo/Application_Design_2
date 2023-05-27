03dd1ca342cee0cf4b4961ce2bec4f44c6ca0459
carmesotuyo <carmela.sotuyo@gmail.com>
2023-05-27 11:23:50 -0300

    updated busines logic interfaces comment & deleted reply

--- Codigo/Backend/BlogsApp.IBusinessLogic/Interfaces/ICommentLogic.cs
+++ Codigo/Backend/BlogsApp.IBusinessLogic/Interfaces/ICommentLogic.cs
@@ -7,6 +7,5 @@ namespace BlogsApp.IBusinessLogic.Interfaces
         Comment CreateComment(Comment comment, User loggedUser);
         void DeleteComment(int commentId, User loggedUser);
         IEnumerable<Comment> GetCommentsSince(User loggedUser, DateTime? lastLogout);
-        Reply AddReply(Comment comment, Reply reply, User loggedUser);
     }
 }
--- Codigo/Backend/BlogsApp.IBusinessLogic/Interfaces/IReplyLogic.cs
+++
@@ -1,9 +0,0 @@
-﻿using BlogsApp.Domain.Entities;
-namespace BlogsApp.IBusinessLogic.Interfaces
-{
-    public interface IReplyLogic
-    {
-        Reply CreateReply(Reply reply, User loggedUser);
-        void DeleteReply(int commentId, User loggedUser);
-    }
-}
