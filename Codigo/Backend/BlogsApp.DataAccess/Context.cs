23be32d1112bf7225351bb873bb8f24f302f0dfa
carmesotuyo <carmela.sotuyo@gmail.com>
2023-05-27 11:05:51 -0300

    removed replies from DataAccess - pending Migration

--- Codigo/Backend/BlogsApp.DataAccess/Context.cs
+++ Codigo/Backend/BlogsApp.DataAccess/Context.cs
@@ -11,7 +11,6 @@ namespace BlogsApp.DataAccess
         public DbSet<User> Users { get; set; }
         public DbSet<Article> Articles { get; set; }
         public DbSet<Comment> Comments { get; set; }
-        public DbSet<Reply> Replies { get; set; }
         public DbSet<Session> Sessions { get; set; }
         public DbSet<LogEntry> LogEntries { get; set; }
 
@@ -31,8 +30,8 @@ namespace BlogsApp.DataAccess
                  .AddJsonFile("appsettings.json")
                  .Build();
 
-                //var connectionString = configuration.GetConnectionString(@"BlogsAppDBCarme");
-                var connectionString = configuration.GetConnectionString(@"BlogsAppDBFer");
+                var connectionString = configuration.GetConnectionString(@"BlogsAppDBCarme");
+                //var connectionString = configuration.GetConnectionString(@"BlogsAppDBFer");
                 //var connectionString = configuration.GetConnectionString(@"BlogsAppDBGime");
 
                 optionsBuilder.UseSqlServer(connectionString!);
