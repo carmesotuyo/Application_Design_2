37fa5428f29abe473cc6eb40423337894c465985
carmesotuyo <carmela.sotuyo@gmail.com>
2023-06-08 16:11:15 -0300

    removed id from logout session controller

--- Codigo/Backend/BlogsApp.DataAccess/Context.cs
+++ Codigo/Backend/BlogsApp.DataAccess/Context.cs
@@ -29,8 +29,8 @@ namespace BlogsApp.DataAccess
                  .AddJsonFile("appsettings.json")
                  .Build();
 
-                //var connectionString = configuration.GetConnectionString(@"BlogsAppDBCarme");
-                var connectionString = configuration.GetConnectionString(@"BlogsAppDBFer");
+                var connectionString = configuration.GetConnectionString(@"BlogsAppDBCarme");
+                //var connectionString = configuration.GetConnectionString(@"BlogsAppDBFer");
                 //var connectionString = configuration.GetConnectionString(@"BlogsAppDBGime");
 
                 optionsBuilder.UseSqlServer(connectionString!);
