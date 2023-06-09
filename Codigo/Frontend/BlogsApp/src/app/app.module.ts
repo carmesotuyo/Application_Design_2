68efbea78328836cc1b3be8618aa7e5f04957e15
Fernando Spillere <f.spillere269@gmail.com>
2023-06-09 01:19:16 -0300

    creacion de interceptors auth

--- Codigo/Frontend/BlogsApp/src/app/app.module.ts
+++ Codigo/Frontend/BlogsApp/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { LoginService } from './services/login.service';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -13,6 +13,8 @@ import { HomeComponent } from './components/home/home.component';
 import { AuthService } from './services/auth.service';
 import { ArticleService } from './services/article.service';
 import { ArticleFilterPipe } from './components/home/pipes/article-filter.pipe';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -31,7 +33,17 @@ import { ArticleFilterPipe } from './components/home/pipes/article-filter.pipe';
   providers: [
     LoginService,
     AuthService,
-    ArticleService
+    ArticleService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ResponseInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
   ],
   bootstrap: [AppComponent]
 })
