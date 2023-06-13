d866f4b1cc28cd6b96096a60283cfbec4822c3bd
Fernando Spillere <f.spillere269@gmail.com>
2023-06-13 01:40:04 -0300

    fix de get articles

--- Codigo/Backend/BlogsApp.WebAPI/Controllers/CommmentController.cs
+++ Codigo/Backend/BlogsApp.WebAPI/Controllers/CommmentController.cs
@@ -27,7 +27,7 @@ namespace BlogsApp.WebAPI.Controllers
             Article article = articleLogic.GetArticleById(comment.ArticleId, loggedUser);
             Comment newComment = CommentConverter.FromDto(comment, loggedUser, article);
             Comment createdCommented = commentLogic.CreateComment(newComment, loggedUser);
-            return new OkObjectResult(CommentConverter.toDto(createdCommented));
+            return new OkObjectResult(CommentConverter.toBasicDto(createdCommented));
         }
 
         [HttpPost("{parentCommentId}")]
@@ -39,7 +39,7 @@ namespace BlogsApp.WebAPI.Controllers
             Comment parentComment = commentLogic.GetCommentById(parentCommentId);
             Comment newComment = CommentConverter.FromDto(comment, loggedUser, article);
             Comment createdComment = commentLogic.ReplyToComment(parentComment, newComment, loggedUser);
-            return new OkObjectResult(CommentConverter.toDto(createdComment));
+            return new OkObjectResult(CommentConverter.toBasicDto(createdComment));
         }
     }
 }
